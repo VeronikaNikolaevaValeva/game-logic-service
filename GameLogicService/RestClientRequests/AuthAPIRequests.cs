e6aedb04cf87d20122e5ce6ced900b00c7a3eb57
Veronika Valeva <v.valeva@student.fontys.nl>
2023-06-01 04:07:59 +0200

    /

--- GameLogicService/RestClientRequests/AuthAPIRequests.cs
+++ GameLogicService/RestClientRequests/AuthAPIRequests.cs
@@ -30,21 +30,21 @@ namespace GameLogicService.RestClientRequests
         {
             try
             {
-                var client = new RestClient($"https://dev-he67eqpc846lev05.us.auth0.com/api/v2/users/{authId}");
-                var request = new RestRequest(Method.Patch.ToString());
-                request.AddHeader("content-type", "application/json");
-                request.AddHeader("authorization", $"Bearer {token}");
-                request.AddHeader("cache-control", "no-cache");
-                request.AddParameter("application/json", "{ \"scopes\": [ { \"value\": \"delete:users\", \"description\": \"Delete Users\" }, { \"value\": \"delete:users\", \"description\": \"Delete Users\" } ] }", ParameterType.RequestBody);
-                var response = client.Execute(request);
+                //var client = new RestClient($"https://dev-he67eqpc846lev05.us.auth0.com/api/v2/users/{authId}");
+                //var request = new RestRequest(Method.Patch.ToString());
+                //request.AddHeader("content-type", "application/json");
+                //request.AddHeader("authorization", $"Bearer {token}");
+                //request.AddHeader("cache-control", "no-cache");
+                //request.AddParameter("application/json", "{ \"scopes\": [ { \"value\": \"delete:users\", \"description\": \"Delete Users\" }, { \"value\": \"delete:users\", \"description\": \"Delete Users\" } ] }", ParameterType.RequestBody);
+                //var response = client.Execute(request);
 
 
-             //   _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("WWW-Authenticate", string.Format("Bearer {0}", token));
-             //   var response = await _httpClient.DeleteAsync($"users/{authId}");
-             //Console.WriteLine(response.StatusCode.ToString());
-             //Console.WriteLine(response.Content.ToString());
-             //Console.WriteLine(response.Headers.ToString());
-             //Console.WriteLine(response.ReasonPhrase.ToString());
+                _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("authorization", string.Format("Bearer {0}", token));
+                var response = await _httpClient.DeleteAsync($"users/{authId}");
+                Console.WriteLine(response.StatusCode.ToString());
+                Console.WriteLine(response.Content.ToString());
+                Console.WriteLine(response.Headers.ToString());
+                Console.WriteLine(response.ReasonPhrase.ToString());
             }
             catch(Exception ex) 
             {
