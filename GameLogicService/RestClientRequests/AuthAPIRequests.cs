1ef94b692b2243a1894ee251f9aab00ebe674b49
Veronika Valeva <v.valeva@student.fontys.nl>
2023-06-01 16:07:08 +0200

    .

--- GameLogicService/RestClientRequests/AuthAPIRequests.cs
+++ GameLogicService/RestClientRequests/AuthAPIRequests.cs
@@ -40,21 +40,23 @@ namespace GameLogicService.RestClientRequests
                 request.AddHeader("content-type", "application/json");
                 request.AddParameter("application/json", "{\"client_id\":\"5dVfPMbskP7LMi0nIXtVou5KUdxoxnSQ\",\"client_secret\":\"Uqz-umF1JmKWukJ9OY9b45vTKG5bCpgy9C7vtSAt40N-LZccZzTkpdIJzpao3KC1\",\"audience\":\"https://dev-he67eqpc846lev05.us.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
                 var response = client.Execute(request);
-                Console.WriteLine(response);
+                Console.WriteLine(response.StatusCode.ToString());
+                Console.WriteLine(response.Content.ToString());
+
 
                 //var managementApi = new ManagementApiClient(token, new Uri("https://dev-he67eqpc846lev05.us.auth0.com/"), new HttpClientManagementConnection());
                 //await managementApi.Users.DeleteAsync(authId);
                 //using (var client = new HttpClient())
                 //{
-                    //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
-                    //client.BaseAddress = new Uri("https://dev-he67eqpc846lev05.us.auth0.com");
-                    //var result = await client.DeleteAsync("/api/v2/users/" + authId);
-                    //string resultContent = await result.Content.ReadAsStringAsync();
-                    //var response = JObject.Parse(resultContent);
-                    //Console.WriteLine(result.StatusCode.ToString());
-                    //Console.WriteLine(result.Content.ToString());
-                    //Console.WriteLine(resultContent.ToString());
-                    //Console.WriteLine(response.ToString());
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
+                //client.BaseAddress = new Uri("https://dev-he67eqpc846lev05.us.auth0.com");
+                //var result = await client.DeleteAsync("/api/v2/users/" + authId);
+                //string resultContent = await result.Content.ReadAsStringAsync();
+                //var response = JObject.Parse(resultContent);
+                //Console.WriteLine(result.StatusCode.ToString());
+                //Console.WriteLine(result.Content.ToString());
+                //Console.WriteLine(resultContent.ToString());
+                //Console.WriteLine(response.ToString());
                 //}
             }
             catch (Exception ex)
