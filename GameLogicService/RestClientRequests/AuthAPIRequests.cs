e47ec6957521d11542b1b8cd4012925ba400943c
Veronika Valeva <v.valeva@student.fontys.nl>
2023-06-01 15:28:41 +0200

    .

--- GameLogicService/RestClientRequests/AuthAPIRequests.cs
+++ GameLogicService/RestClientRequests/AuthAPIRequests.cs
@@ -5,6 +5,9 @@ using System.Net.Http.Headers;
 using GameLogicService.Models.Responses;
 using Azure;
 using RestSharp;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace GameLogicService.RestClientRequests
 {
@@ -28,16 +31,29 @@ namespace GameLogicService.RestClientRequests
         {
             try
             {
-                _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("authorization", string.Format("Bearer {0}", token));
-                _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/json");
-                _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("cache-control", "no-cache");
-                var response = await _httpClient.DeleteAsync($"/{authId}");
-                Console.WriteLine(response.StatusCode.ToString());
-                Console.WriteLine(response.Content.ToString());
-                Console.WriteLine(response.Headers.ToString());
-                Console.WriteLine(response.ReasonPhrase.ToString());
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    client.BaseAddress = new Uri("https://dev-he67eqpc846lev05.us.auth0.com");
+                    var result = await client.DeleteAsync("/api/v2/users/" + authId);
+                    string resultContent = await result.Content.ReadAsStringAsync();
+                    var response = JObject.Parse(resultContent);
+                    Console.WriteLine(result.StatusCode.ToString());
+                    Console.WriteLine(result.Content.ToString());
+                    Console.WriteLine(resultContent.ToString());
+                    Console.WriteLine(response.ToString());
+                }
+
+                //_httpClient.DefaultRequestHeaders.TryAddWithoutValidation("authorization", string.Format("Bearer {0}", token));
+                //_httpClient.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/json");
+                //_httpClient.DefaultRequestHeaders.TryAddWithoutValidation("cache-control", "no-cache");
+                //var response = await _httpClient.DeleteAsync($"/{authId}");
+                //Console.WriteLine(response.StatusCode.ToString());
+                //Console.WriteLine(response.Content.ToString());
+                //Console.WriteLine(response.Headers.ToString());
+                //Console.WriteLine(response.ReasonPhrase.ToString());
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return false;
